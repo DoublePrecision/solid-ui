e533e3fb347cc4ac229c14ef1124007b250ddad6
Budi Syahiddin <me@inve.rs>
2026-03-02 05:34:48 +0800

    fix: error 500 on prod

--- apps/docs/src/entry-server.tsx
+++ apps/docs/src/entry-server.tsx
@@ -7,11 +7,6 @@ export default createHandler(() => (
       <html lang="en">
         <head>
           {assets}
-          <script
-            defer
-            src="http://umami-a8gg4kg4co40owco8w0so0k0.91.99.20.236.sslip.io/script.js"
-            data-website-id="19d584f1-8277-4761-8b29-ea2dc5521a75"
-          />
           <link
             rel="preload"
             href="/fonts/Geist.woff2"
@@ -27,7 +22,7 @@ export default createHandler(() => (
             crossorigin="anonymous"
           />
         </head>
-        <body class="min-h-screen bg-background font-sans antialiased">
+        <body class="bg-background min-h-screen font-sans antialiased">
           <div id="app">{children}</div>
           {scripts}
         </body>
